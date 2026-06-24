6db5aaa0847fa5d8118fa687aadbe161443805b0
Md. Misskhat Hossen Milon <misskhat.hossen.com>
2026-06-25 00:20:41 +0600

    large number problem solve

--- taskSolve.js
+++ taskSolve.js
@@ -1,16 +1,41 @@
 // Problem: 1
 // Prime Number Check: Given a number 29, explain how you would determine whether it is a prime number or not. Input: 29 Output: Prime Number
 
-const number = 29;
-const number2 = 18;
-const number3 = 16;
-const number4 = 32;
-const number5 = 100;
-
 function primeNumberChecker(n) {
+  //n is a number or not vValidation check
+  if (n !== "number") console.log("Please provide a number");
+
+  //prime number logic:
   if (n % n === 0 && n % 2 !== 0) {
     console.log("Prime  Number.");
   } else {
     console.log("Not Prime Number.");
   }
+}
+
+// Problem: 2
+// Find Largest Element in Array: Given an array [12, 45, 7, 89, 23], explain how you would find the largest element efficiently. Input: [12, 45, 7, 89, 23] Output: 89
+
+function largeElementInArray(arr) {
+  //is it array or not validation check.
+  if (!Array.isArray(arr)) {
+    console.log("Please provide the array");
+    return;
+  }
+
+  //array all elements are numbers or not validation check.
+  const isArrayValid = arr.every((i) => typeof i === "number" && !isNaN(i));
+  if (!isArrayValid) {
+    console.log("Please provide a full number's array");
+    return;
+  }
+
+  //large element logic:
+  let largeNumber = 0;
+  for (const n of arr) {
+    if (n > largeNumber) {
+      largeNumber = n;
+    }
+  }
+  console.log(largeNumber);
 }
